804d4c1be6b4d9ecd5a01e2d3bc3a966b84b4a08
xiaowei.hu <Xiaowei.Hu@lianwei.com.cn>
2025-10-19 09:48:32 +0800

    fix(wxml): 订单确认页去除表达式计算，改为JS预计算feeY避免WXML编译错误

--- miniprogram/projects/placeone/pages/lesson/order_confirm/lesson_order_confirm.js
+++ miniprogram/projects/placeone/pages/lesson/order_confirm/lesson_order_confirm.js
@@ -3,7 +3,7 @@ const ProjectBiz = require('../../../biz/project_biz.js');
 
 Page({
 	data:{ coachId:'', courseId:'', venueId:'', date:'', start:'', end:'', endPoint:'', fee:0 },
-	onLoad(options){ ProjectBiz.initPage(this, { title: '确认订单' }); this.setData(options); },
+	onLoad(options){ ProjectBiz.initPage(this, { title: '确认订单' }); this.setData(options); this.setData({ feeY: (Number(this.data.fee)/100).toFixed(2) }); },
 	async submit(){
 		let { coachId, courseId, venueId, date, start, end, endPoint, fee } = this.data;
 		let ret = await cloudHelper.callCloudSumbit('lesson/order_create', { coachId, courseId, venueId, date, start, end, endPoint, fee: Number(fee), forms: [] }, { title: '提交中' });
